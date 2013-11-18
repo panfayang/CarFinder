28d49afb57bb7f672ff315ab7a5226bbaac22dac
pan <panfayang@gmail.com>
2013-11-18 04:09:39 -0500

    carFinder

---
+++ src/com/example/carfinder/CarMap.java
@@ -0,0 +1,72 @@
+package com.example.carfinder;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CarMap extends FragmentActivity { 
+	GoogleMap mMap;
+	private String mKeyCurLat;
+	private String mKeyCurLng;
+	private String mKeyCarLat;
+	private String mKeyCarLng;
+
+	SharedPreferences mPrefs;
+	SharedPreferences.Editor mEditor;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_car_map);
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mPrefs = this.getSharedPreferences("com.example.carfinder", Context.MODE_PRIVATE);
+
+		mKeyCarLat = "carLocationLat";
+		mKeyCarLng = "carLocationLng";
+		mKeyCurLat = "currentLocationLat";
+		mKeyCurLng = "currentLocationLng";
+
+		if (mMap!=null){
+			mMap.setMyLocationEnabled(true);
+
+			double curLat = Double.valueOf(mPrefs.getString(mKeyCurLat, "0"));
+			double curLng = Double.valueOf(mPrefs.getString(mKeyCurLng, "0"));
+
+			LatLng curLoc = new LatLng(curLat,curLng);		
+			mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLoc, 15));
+
+			double carLat = Double.valueOf(mPrefs.getString(mKeyCarLat, "0"));
+			double carLng = Double.valueOf(mPrefs.getString(mKeyCarLng, "0"));
+			if (carLng!=0.0){
+				LatLng carLoc = new LatLng(carLat,carLng);
+
+				mMap.addMarker(new MarkerOptions()
+				.position(carLoc)
+				.title("Here's your car"));
+			}
+		}
+	}
+
+
+//	public void putMarker(){
+//
+//		if (mMap!=null){
+//			double lat = Double.valueOf(mPrefs.getString(mKeyCarLat, "0"));
+//			double lng = Double.valueOf(mPrefs.getString(mKeyCarLng, "0"));
+//
+//			LatLng carLoc = new LatLng(lat,lng);
+//
+//			mMap.addMarker(new MarkerOptions()
+//			.position(carLoc)
+//			.title("Here's your car"));
+//		}
+//	}
+
+}
---
+++ src/com/example/carfinder/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.carfinder;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+public class MainActivity extends FragmentActivity 
+implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener
+
+{
+
+	private String mKeyCarLat;
+	private String mKeyCarLng;
+
+	private String mKeyCurLat;
+	private String mKeyCurLng;
+
+
+	LocationClient mLocationClient;
+	Location mCurrentLocation;
+
+	SharedPreferences mPrefs;
+	SharedPreferences.Editor mEditor;
+
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//		mPrefs = this.getSharedPreferences("com.example.carfinder", Context.MODE_PRIVATE);
+		mKeyCarLat = "carLocationLat";
+		mKeyCarLng = "carLocationLng";
+		mKeyCurLat = "currentLocationLat";
+		mKeyCurLng = "currentLocationLng";
+
+		mLocationClient = new LocationClient(this, this, this);
+	}
+
+
+	public void viewMap(View view){
+		Intent intent = new Intent(this,CarMap.class);
+		startActivity(intent);
+	}
+
+	public static class ErrorDialogFragment extends DialogFragment{
+		private Dialog mDialog;
+
+		public ErrorDialogFragment(){
+			super();
+			mDialog = null;
+		}
+
+		public void setDialog(Dialog dialog){
+			mDialog = dialog;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		switch (requestCode){
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+			switch (resultCode){
+			case Activity.RESULT_OK:
+				break;
+			}
+		}
+	}
+
+	private boolean servicesConnected(){
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (ConnectionResult.SUCCESS == resultCode){
+			Log.d("Location Updates", "Google Play Services is available");
+
+			return true;
+		}else{
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(resultCode,this,CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+			if (errorDialog != null) {
+				// Create a new DialogFragment for the error dialog
+				ErrorDialogFragment errorFragment =
+						new ErrorDialogFragment();
+				// Set the dialog in the DialogFragment
+				errorFragment.setDialog(errorDialog);
+				// Show the error dialog in the DialogFragment
+				errorFragment.show(getSupportFragmentManager(),
+						"Location Updates");
+			}
+			return false;
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		getCurLoc(null);
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects.
+		 * If the error has a resolution, try sending an Intent to
+		 * start a Google Play services activity that can resolve
+		 * error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(
+						this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	public void getCarLoc(View view){
+		mCurrentLocation = mLocationClient.getLastLocation();
+		double lat = mCurrentLocation.getLatitude();
+		double lng = mCurrentLocation.getLongitude();
+		
+		((TextView) findViewById(R.id.latitude)).setText(Double.toString(lat));
+		((TextView) findViewById(R.id.longitude)).setText(Double.toString(lng));
+
+		mPrefs = this.getSharedPreferences("com.example.carfinder", Context.MODE_PRIVATE);
+		mPrefs.edit().putString(mKeyCarLat, Double.toString(lat)).commit();
+		mPrefs.edit().putString(mKeyCarLng, Double.toString(lng)).commit();
+//
+//		CarMap carMap = new CarMap();
+//		carMap.putMarker();
+
+	}
+
+	public void getCurLoc(View view){
+		mCurrentLocation = mLocationClient.getLastLocation();
+		double curLat = mCurrentLocation.getLatitude();
+		double curLng = mCurrentLocation.getLongitude();
+
+		mPrefs = this.getSharedPreferences("com.example.carfinder", Context.MODE_PRIVATE);
+
+		mPrefs.edit().putString(mKeyCurLat, Double.toString(curLat)).commit();
+		mPrefs.edit().putString(mKeyCurLng, Double.toString(curLng)).commit();
+
+	}
+
+} 
